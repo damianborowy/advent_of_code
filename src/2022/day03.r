8c5970299cab7e86195643f54992ea243fc1b6f9
Damian Borowy <tymooteuusz@gmail.com>
2022-12-03 12:26:46 +0100

    refactor day03 solution

--- src/2022/day03.r
+++ src/2022/day03.r
@@ -1,6 +1,3 @@
-# install.packages("tidyverse")
-library(purrr)
-
 input <- 'RCMRQjLLWGTjnlnZwwnZJRZH
 qnvfhpSbvSppNddNdSqbbmmdPrwttJVrVPDVrJtHtwPZhrPJ
 BFpFzSSqSFFSvQsnWgCMjTLzng
@@ -302,34 +299,50 @@ nPPssTBnMJPdtHPVHtRhpv
 bSSgGFWDgWwDFFlmWlcShqdpRqpVcHvvnqpvpRHd
 bGFnGljgSsjBCTBszz'
 
+split_string <- function(text, separator) strsplit(text, separator)[[1]]
+
 rows <- input %>%
-    strsplit(., "\n") %>%
-    .[[1]] %>%
-    map(function(row) strsplit(row, "")[[1]])
+    split_string("\n") %>%
+    sapply(function(row) split_string(row, ""))
 
 map_char_to_ascii_code <- function(char) sapply(char, utf8ToInt)
 
 map_to_item_priorities <- function(items) items %>% 
-    map(map_char_to_ascii_code) %>%
-    map(function(char_code) if (char_code >= 97) char_code - 96 else char_code - 38)
+    sapply(map_char_to_ascii_code) %>%
+    sapply(function(char_code) if (char_code >= 97) char_code - 96 else char_code - 38)
 
 sum_priorities <- function(items) items %>%
-    map(map_to_item_priorities) %>%
-    unlist(recursive=TRUE, use.names=FALSE) %>%
+    sapply(map_to_item_priorities) %>%
+    unlist %>%
     sum
+
+get_intersecting_parts <- function(rows) 
+    sapply(rows, function(parts) Reduce(intersect, parts))
 
 # PART 1
 
+split_rows_in_half <- function(rows) 
+    lapply(
+        rows,
+        function(row) {
+            half_length = length(row) %/% 2
+            
+            list(head(row, half_length), tail(row, half_length))
+        }
+    )
+
 rows %>%
-    map(function(row) list(head(row, length(row) %/% 2), tail(row, length(row) %/% 2))) %>%
-    map(function(parts) Reduce(intersect, parts)) %>%
+    split_rows_in_half %>%
+    get_intersecting_parts %>%
     sum_priorities %>%
     print
 
 # PART 2
 
+chunk_list <- function(list, chunk_size) split(list, ceiling(seq_along(list) / chunk_size))
+
 rows %>%
-    split(ceiling(seq_along(.) / 3)) %>%
-    map(function(parts) Reduce(intersect, parts)) %>%
+    chunk_list(3) %>%
+    get_intersecting_parts %>%
     sum_priorities %>%
     print
