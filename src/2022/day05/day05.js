3fe399bde441ea012be9390d4b4266714dc06d8b
Damian Borowy <damian.borowy@likims.com>
2022-12-05 08:34:31 +0100

    Update day05.js

--- src/2022/day05/day05.js
+++ src/2022/day05/day05.js
@@ -15,7 +15,7 @@ const stacks = fs
 
 const stacksCount = stacks.at(-1).split("").map(Number).filter(Boolean).at(-1);
 
-const currentStacks = [...new Array(stacksCount)].map((_) => []);
+const defaultStacks = [...new Array(stacksCount)].map((_) => []);
 
 for (let i = 0; i < stacks.length - 1; i++) {
     for (let j = 1; j < stacksCount * 4; j += 4) {
@@ -24,33 +24,33 @@ for (let i = 0; i < stacks.length - 1; i++) {
         if (element && element !== " ") {
             const stackIndex = Math.floor(j / 4);
 
-            currentStacks[stackIndex].push(element);
+            defaultStacks[stackIndex].push(element);
         }
     }
 }
 
+const moveStacks = ({ modelNumber }) => {
+    const stacks = defaultStacks.map((stack) => [...stack]);
+
+    steps.forEach(([count, from, to]) => {
+        const shiftedCargo = stacks[from - 1].splice(0, count);
+        const newCargo =
+            modelNumber === 9000 ? shiftedCargo.reverse() : shiftedCargo;
+
+        stacks[to - 1].unshift(...newCargo);
+    });
+
+    return stacks;
+};
+
 // PART 1
 
-const part1Stacks = currentStacks.map((stack) => [...stack]);
-
-steps.forEach(([count, from, to]) => {
-    for (let i = 0; i < count; i++) {
-        const shiftedCargo = part1Stacks[from - 1].shift();
-
-        part1Stacks[to - 1].unshift(shiftedCargo);
-    }
-});
+const part1Stacks = moveStacks({ modelNumber: 9000 });
 
 console.log(part1Stacks.map((stack) => stack[0]).join(""));
 
 // PART 2
 
-const part2Stacks = currentStacks.map((stack) => [...stack]);
-
-steps.forEach(([count, from, to]) => {
-    const shiftedCargo = part2Stacks[from - 1].splice(0, count);
-
-    part2Stacks[to - 1].unshift(...shiftedCargo);
-});
+const part2Stacks = moveStacks({ modelNumber: 9001 });
 
 console.log(part2Stacks.map((stack) => stack[0]).join(""));
