f18de7533fa54d8b353541cc203d9653fcb7cba0
Damian Borowy <damian.borowy@likims.com>
2022-12-06 11:12:23 +0100

    Update day06.lua

--- src/2022/day06/day06.lua
+++ src/2022/day06/day06.lua
@@ -1,32 +1,3 @@
-local function contains(table, element)
-  for _, value in pairs(table) do
-    if value == element then
-      return true
-    end
-  end
-  return false
-end
-
-local function split_string_into_chars(input_string)
-  local result = {}
-
-  for letter in input_string:gmatch(".") do table.insert(result, letter) end
-
-  return result
-end
-
-local function is_every_value_unique(array)
-  local values = {}
-
-  for _key, value in pairs(array) do
-    if contains(values, value) then return false end
-
-    table.insert(values, value)
-  end
-
-  return true
-end
-
 local function read_file(path)
   local file = io.open(path, "rb")
   if not file then return nil end
@@ -41,24 +12,28 @@ local input = read_file("input.txt");
 
 if not input then return nil end
 
-local function find_marker(target_length)
-  local current_buffer = {}
-  local total_index = 0
+local function is_every_value_unique(array)
+  local values = {}
 
-  for _key, value in pairs(split_string_into_chars(input)) do
-    total_index = total_index + 1
-    table.insert(current_buffer, value)
+  for _key, value in pairs(array) do
+    if values[value] then return false end
 
-    if #current_buffer == target_length and is_every_value_unique(current_buffer) then
-      break
-    end
-
-    if #current_buffer >= target_length then
-      table.remove(current_buffer, 1)
-    end
+    values[value] = true
   end
 
-  return total_index
+  return true
+end
+
+local function find_marker(target_length)
+  local current_buffer = {}
+
+  for i = 1, #input do
+    table.insert(current_buffer, input:sub(i, i))
+
+    if #current_buffer == target_length and is_every_value_unique(current_buffer) then return i end
+
+    if #current_buffer >= target_length then table.remove(current_buffer, 1) end
+  end
 end
 
 -- PART 1
