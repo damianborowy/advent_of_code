08db4f5c4fd165baca9b6ba1c2d0593b1f02fb07
Damian Borowy <damian.borowy@likims.com>
2022-12-14 23:18:07 +0100

    refactor day14 solution

--- src/2022/day14/day14.php
+++ src/2022/day14/day14.php
@@ -1,92 +1,112 @@
 <?php
 
-global $input_rows;
+class SandCave {
+  const MAX_X = 1000;
+  const MAX_Y = 1000;
+  const START_X = 500;
+  const START_Y = 0;
 
-$stdin = fopen('php://stdin', 'r');
-$input_string = stream_get_contents($stdin);
-$input_rows = explode("\n", $input_string);
+  public $grid;
 
-function create_grid($with_floor, $input_rows) {
-  $MAX_X = 1000;
-  $MAX_Y = 1000;
-  $grid = array_fill(0, $MAX_Y, array_fill(0, $MAX_X, "."));
-  $floor = 0;
+  public function __construct(public $part, $input_rows) {
+    $this->grid = $this->create_grid($part, $input_rows);
+  }
 
-  for ($row_index = 0; $row_index < count($input_rows); $row_index++) {
-    $point_pairs = explode(" -> ", $input_rows[$row_index]);
-    $points_in_line = [];
-    
-    for ($pair_index = 0; $pair_index < count($point_pairs) - 1; $pair_index++) {
-      $start = explode(",", $point_pairs[$pair_index]);
-      $end = explode(",", $point_pairs[$pair_index + 1]);
-      $start_x = intval($start[0]);
-      $start_y = intval($start[1]);
-      $end_x = intval($end[0]);
-      $end_y = intval($end[1]);
-      $min_x = min($start_x, $end_x);
-      $max_x = max($start_x, $end_x);
-      $min_y = min($start_y, $end_y);
-      $max_y = max($start_y, $end_y);
-      $floor = max($floor, $max_y);
+  function create_grid($part, $input_rows) {
+    $grid = array_fill(0, self::MAX_Y, array_fill(0, self::MAX_X, "."));
+    $floor = 0;
 
-      if ($start_x == $end_x) {
-        for ($y = $min_y; $y <= $max_y; $y++) {
-          $grid[$start_x][$y] = "#";
+    for ($row_index = 0; $row_index < count($input_rows); $row_index++) {
+      $point_pairs = explode(" -> ", $input_rows[$row_index]);
+      $points_in_line = [];
+
+      for ($pair_index = 0; $pair_index < count($point_pairs) - 1; $pair_index++) {
+        [$start_x, $start_y, $end_x, $end_y] = $this->get_point_pairs_indices($point_pairs, $pair_index);
+        [$min_x, $max_x] = $this->get_min_max($start_x, $end_x);
+        [$min_y, $max_y] = $this->get_min_max($start_y, $end_y);
+        $floor = max($floor, $max_y);
+
+        if ($start_x == $end_x) {
+          for ($y = $min_y; $y <= $max_y; $y++) {
+            $grid[$start_x][$y] = "#";
+          }
         }
-      } else if ($start_y == $end_y) {
-        for ($x = $min_x; $x <= $max_x; $x++) {
-          $grid[$x][$start_y] = "#";
+        elseif ($start_y == $end_y) {
+          for ($x = $min_x; $x <= $max_x; $x++) {
+            $grid[$x][$start_y] = "#";
+          }
         }
       }
     }
-  }
 
-  if ($with_floor) {
-    for ($x = 0; $x < $MAX_X; $x++) {
-      $grid[$x][$floor + 2] = "#";
+    if ($part == "part2") {
+      for ($x = 0; $x < self::MAX_X; $x++) {
+        $grid[$x][$floor + 2] = "#";
+      }
     }
+
+    return $grid;
   }
 
-  return $grid;
+  private function get_min_max($value1, $value2) {
+    return [min($value1, $value2), max($value1, $value2)];
+  }
+
+  private function get_point_pairs_indices($point_pairs, $pair_index) {
+    $start = explode(",", $point_pairs[$pair_index]);
+    $end = explode(",", $point_pairs[$pair_index + 1]);
+    $start_indices = array_map("intval", $start);
+    $end_indices = array_map("intval", $end);
+
+    return [...$start_indices, ...$end_indices];
+  }
+
+  function drop_sand() {
+    $count = 0;
+    $x = self::START_X;
+    $y = self::START_Y;
+
+    while (true) {
+      if ($y + 1 == self::MAX_Y && $this->part == "part1") {
+        break;
+      }
+
+      if ($this->grid[$x][$y + 1] == ".") {
+        $y++;
+      }
+      elseif ($x > 0 && $this->grid[$x - 1][$y + 1] == ".") {
+        $x--;
+        $y++;
+      }
+      elseif ($x < self::MAX_X - 1 && $this->grid[$x + 1][$y + 1] == ".") {
+        $x++;
+        $y++;
+      }
+      else {
+        $this->grid[$x][$y] = "o";
+        $count++;
+
+        if ($x == self::START_X && $y == self::START_Y && $this->part == "part2") {
+          break;
+        }
+
+        $x = self::START_X;
+        $y = self::START_Y;
+      }
+    }
+
+    return $count;
+  }
 }
 
-function drop_sand($part, $input_rows) {
-  $START_X = 500;
-  $START_Y = 0;
-  $MAX_X = 1000;
-  $MAX_Y = 1000;
-  $grid = create_grid($part == "part2", $input_rows);
-  $count = 0;
-  $x = $START_X;
-  $y = $START_Y;
+$stdin = fopen("php://stdin", "r");
+$input_string = stream_get_contents($stdin);
+$input_rows = explode("\n", $input_string);
 
-  while (true) {
-    if ($y + 1 == $MAX_Y && $part == "part1") {
-      break;
-    }
-    
-    if ($grid[$x][$y + 1] == ".") $y++;
-    else if ($x > 0 && $grid[$x - 1][$y + 1] == ".") {
-      $x--;
-      $y++;
-    } else if ($x < ($MAX_X - 1) && $grid[$x + 1][$y + 1] == ".") {
-      $x++;
-      $y++;
-    } else {
-      $grid[$x][$y] = "o";
-      $count++;
+$part1Cave = new SandCave("part1", $input_rows);
+print "Part 1: " . $part1Cave->drop_sand() . "\n";
 
-      if ($x == $START_X && $y == $START_Y && $part == "part2") break;
-
-      $x = $START_X;
-      $y = $START_Y;
-    }
-  }
-
-  return $count;
-}
-
-print("Part 1: " . drop_sand("part1", $input_rows) . "\n");
-print("Part 2: " . drop_sand("part2", $input_rows));
+$part2Cave = new SandCave("part2", $input_rows);
+print "Part 2: " . $part2Cave->drop_sand();
 
 ?>
